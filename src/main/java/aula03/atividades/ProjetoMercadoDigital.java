92c5ccda0c51a831b92948c504014e0cf2f3d147
jefersonrgomes <jeferson.rgomes@hotmail.com>
2022-04-04 18:11:11 -0300

    Changes by thiago

--- src/main/java/aula03/atividades/ProjetoMercadoDigital.java
+++ src/main/java/aula03/atividades/ProjetoMercadoDigital.java
@@ -3,6 +3,7 @@ package aula03.atividades;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ProjetoMercadoDigital {
@@ -39,12 +40,11 @@ public class ProjetoMercadoDigital {
  Conhecendo a interface Map<k,v>
  https://www.devmedia.com.br/conhecendo-a-interface-map-do-java/37463
  https://www.alura.com.br/artigos/iterando-por-um-hashmap-em-java?gclid=Cj0KCQjw6J-SBhCrARIsAH0yMZjWlIxQjs5YxdCBoQFQlz1BzMRVEnwH0ab2HRf0mVmGnnBq_DS75ckaAvOfEALw_wcB
-
  ***/
         //VARIAVEIS DE CONTROLE PARA OS ITENS DO MERCADO
         String[] produtos = {"Leite", "Cereal", "Arroz", "Atum", "Feijão", "Azeite", "Óleo", "Sabão", "Sal", "Açucar"};
-        Integer [] estoque = {10,10,10,10,10,10,10,10,10,10};
-        Float [] precos = {4.57f, 3.02f,9.43f,3.55f,6.55f,4.55f,7.33f,1.99f,3.82f,4.29f };
+        Integer[] estoque = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        Float[] precos = {4.57f, 3.02f, 9.43f, 3.55f, 6.55f, 4.55f, 7.33f, 1.99f, 3.82f, 4.29f};
 
         //VARIAVEIS DE CONTROLE PARA CARRINHO DE COMPRA
         ArrayList<Integer> carrinhoCodigoProduto = new ArrayList<>();
@@ -60,17 +60,18 @@ public class ProjetoMercadoDigital {
     // PRATILEIRA DO MERCADO
     public static void pratileiraMercado(
             String[] produtos,
-            Integer [] estoque,
-            Float [] precos,
+            Integer[] estoque,
+            Float[] precos,
             ArrayList<Integer> carrinhoCodigoProduto,
-            ArrayList<Integer> carrinhoQuantidade)
-    {
+            ArrayList<Integer> carrinhoQuantidade) {
+        Integer[] estoqueCopia = Arrays.copyOf(estoque, estoque.length);
         boolean checker = false;
         do {
 
             int choice;
             int code = 0, quantidade = 0;
-
+            //System.out.printf("'%5.2f'%n", 5.1473);
+            //5.15
             System.out.printf("%n\t\t\t WIPRO STORE%n");
             System.out.println("=======================================================================");
             System.out.printf("CODIGO\t\tPRODUTO \t QUANTIDADE \t PREÇO %n");
@@ -84,13 +85,11 @@ public class ProjetoMercadoDigital {
             if (code >= produtos.length || code < 0) {
                 System.out.println("O produto informado não esta na lista no momento, selecione outro produto");
                 continue;
-            }
-            else
-            {
+            } else {
                 carrinhoCodigoProduto.add(code);
             }
 
-            quantidade = validaEntradaDoUsuarioSeInteiro("\nQTN PROD\nDigite a quantidade que deseja colocar no carrinho: ", "\nERROR - Valor Invalido\nInforme um valor numerico entre 1 a 10");
+            quantidade = validaEntradaDoUsuarioSeInteiro("\nQTN PROD\nDigite a quantidade que deseja colocar no carrinho: ", "\nERROR - Valor Invalido\nInforme um valor numerico entre 1 a 10", "[0-9]*");
 
             if (estoque[code] == 0) {
                 System.out.println("A quantidade informada é maior do que o numero disponivel em estoque");
@@ -98,9 +97,7 @@ public class ProjetoMercadoDigital {
             } else if (quantidade > estoque[code]) {
                 System.out.println("A quantidade informada é maior do que o numero disponivel em estoque");
                 continue;
-            }
-            else
-            {
+            } else {
                 carrinhoQuantidade.add(quantidade);
                 estoque[code] -= quantidade;
             }
@@ -108,101 +105,93 @@ public class ProjetoMercadoDigital {
             System.out.println("\nDeseja continuar as compras?\n");
             System.out.printf("--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---%n");
             choice = validaEntradaDoUsuarioSeInteiro(
-            "Opção Invalida",
-            "[1] - Incluir mais itens no seu carrido.\n[2] - Ir para carrinho de compras e formas de pagamento.\n",
-            "--- --- --- --- --- --- --- --- --- ---",
-            "[1-2]" );
+                    "[1] - Incluir mais itens no seu carrido.\n[2] - Ir para carrinho de compras e formas de pagamento.\n",
+                    "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---",
+                    "[1-2]",
+                    "Opção Invalida");
 
             if (choice == 1) {
                 System.out.println("Voltando a prateleira de produtos");
                 continue;
-            }
-            else if(choice == 2)
-            {
-                carrinhoDeCompras(carrinhoCodigoProduto,carrinhoQuantidade,produtos,precos);
-                checker = true;
-            }
-        }while(!checker);
-    }
+            } else if (choice == 2) {
+                boolean finish = carrinhoDeCompras(carrinhoCodigoProduto, carrinhoQuantidade, produtos, precos, estoque, estoqueCopia);
+                estoque = finish ? estoqueCopia : estoque;
+                System.out.println("Deseja voltar para a pratileira de compras?");
+                System.out.printf("--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---%n");
+                choice = validaEntradaDoUsuarioSeInteiro(
+                        "\n\n[1] - Sim refazer compras.\n[2] - Não, sair do mercado.\n\n",
+                        "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---",
+                        "[1-2]",
+                        "Opção Invalida");
 
+                if (choice == 1) {
+                    System.out.println("Voltando a prateleira de produtos");
+                    continue;
+                } else {
+
+                    checker = true;
+                }
+            }
+
+        } while (!checker);
+    }
 
 
     //METODO VALIDA ENTRADA DO USUARIO SE É NUMERO INTEIRO - 1
     public static int validaEntradaDoUsuarioSeInteiro(
             String textIn,
-            String textOut)
-    {
-        Scanner nv = new Scanner(System.in);
-        String valorDigitado;
-        boolean checker=false;
-        int inteiroRetorno=0;
-
-        do {
-            System.out.println(textIn);
-            valorDigitado=nv.next();
-            if(valorDigitado.matches("[0-9]*")) {
-                inteiroRetorno = Integer.parseInt(valorDigitado);
-                checker = true;
-            }
-            else{
-                System.out.println(textOut);
-                continue;
-            }
-        }while(!checker);
-        return inteiroRetorno;
+            String textOut,
+            String regex) {
+        return validaEntradaDoUsuarioSeInteiro(textIn, textOut, regex, "");
     }
 
     //METODO VALIDA ENTRADA DO USUARIO SE É NUMERO INTEIRO - 2
     public static int validaEntradaDoUsuarioSeInteiro(
-            String txtAlert,
             String textIn,
             String textOut,
-            String regex)
-    {
+            String regex,
+            String txtAlert) {
         Scanner nv = new Scanner(System.in);
         String valorDigitado;
-        boolean checker=false;
-        int inteiroRetorno=0;
+        boolean checker = false;
+        int inteiroRetorno = 0;
 
         do {
             System.out.println(textIn);
-            valorDigitado=nv.next();
-            if(valorDigitado.matches(regex)) {
+            valorDigitado = nv.next();
+            if (valorDigitado.matches(regex)) {
                 inteiroRetorno = Integer.parseInt(valorDigitado);
                 checker = true;
-            }
-            else{
+            } else {
                 System.out.println(textOut);
                 System.out.println(txtAlert);
                 continue;
             }
-        }while(!checker);
-        return inteiroRetorno;    }
+        } while (!checker);
+        return inteiroRetorno;
+    }
 
     // METODO CARRINHO DE COMPRAS
     public static boolean carrinhoDeCompras(@NotNull ArrayList<Integer> carrinhoCodigoProduto, ArrayList<Integer> carrinhoQuantidade, String[] produtos, Float[] precos, Integer[] estoque, Integer[] estoqueCopia) {
 
         float valorTotalDeCompra = 0;
-        float valorImposto=0;
-        System.out.printf("\t\t ITENS NO CARRINHO%n"+
-                "--- --- --- --- --- --- --- --- --- ---%n"+
-                "PRODUTO \tPREÇO \tQUANTIDADE \tTOTAL%n");
-        for(int i = 0; i < carrinhoCodigoProduto.size(); i++)
-        {
-            System.out.printf("%s\t\t%s\t\t%s\t\t%s%n",
+        float valorImposto = 0;
+        System.out.printf("%nITENS NO CARRINHO%n" +
+                "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---%n" +
+                "Nome Prod. \tQtde. no Carrinho \tPreço Unit. \tPreço Total%n");
+        for (int i = 0; i < carrinhoCodigoProduto.size(); i++) {
+            System.out.printf("%s\t\t\t%s\t\t\t\t RS %5.2f\t\t R$ %5.2f%n",
                     produtos[carrinhoCodigoProduto.get(i)],
+                    carrinhoQuantidade.get(i),
                     precos[carrinhoCodigoProduto.get(i)],
                     precos[carrinhoCodigoProduto.get(i)] * carrinhoQuantidade.get(i)
             );
             valorTotalDeCompra += (precos[carrinhoCodigoProduto.get(i)] * carrinhoQuantidade.get(i));
-            valorImposto = (9.0f / 100.0f) *valorTotalDeCompra;
+            valorImposto = (9.0f / 100.0f) * valorTotalDeCompra;
 
         }
-        System.out.println("\t\t TOTAL ");
-        System.out.printf("TOTAL - O valor total da compra R$: %s%n", valorTotalDeCompra + valorImposto, valorImposto);
-        //System.out.printf("IMPOSTOS - Valor total de imposto correspondente a 9 porcento sobre o valor total R$: %s%n", valorImposto);
-        //System.out.printf("TOTAL + IMPOSTOS - O valor total da compra com Imposto de 9 porcento é de R$: %s%n", valorTotalDeCompra + valorImposto);
-        System.out.printf("%n%n--- --- --- --- --- --- --- --- --- ---%n" +
+        System.out.printf("%nTOTAL - O valor total da compra R$: %5.2f%n", valorTotalDeCompra);
+        System.out.printf("%n--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---%n" +
                 "[1] - Escolher forma de pagamento. %n" +
                 "[2] - Cancelar compra e esvaziar carrinho. %n" +
                 "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---%n");
@@ -212,11 +201,9 @@ public class ProjetoMercadoDigital {
         do {
             Scanner sc = new Scanner(System.in);
             String choice = sc.next();
-        if (choice.matches("[1-2]*"))
-        {
-            if (choice.matches("1"))
-            {
-                int payMethod = formasDePagamento();
+            if (choice.matches("[1-2]*")) {
+                if (choice.matches("1")) {
+                    int payMethod = formasDePagamento();
 
                     if (payMethod == 1) {
                         desconto = ((20.0f / 100) * valorTotalDeCompra);
@@ -252,7 +239,7 @@ public class ProjetoMercadoDigital {
                     "%n=======================================================================%n" +
                     "PRODUTO\t\t QTND.PRODUTOS\t\t PREÇO UNIT.\t\t PREÇO TOTAL%n");
                 for (int i = 0; i < carrinhoCodigoProduto.size(); i++) {
-                    System.out.printf("%s\t\t%s\t\t%s\t\t%s%n",
+                    System.out.printf("%s\t\t\t\t %s\t\t\t %5.2f\t\t\t\t %5.2f %n",
                             produtos[carrinhoCodigoProduto.get(i)],
                             carrinhoQuantidade.get(i),
                             precos[carrinhoCodigoProduto.get(i)],
@@ -270,15 +257,15 @@ public class ProjetoMercadoDigital {
     }
 
     // METODO FORMAS DE PAGAMENTO
-    public static int formasDePagamento(){
+    public static int formasDePagamento() {
         int choice;
-        choice = validaEntradaDoUsuarioSeInteiro("Opção Invalida\nEscolha entre 1 a 4 uma das opções disponiveis para pagamento\n",
-                "[1] - À vista em dinheiro/Pix ou cartão MASTERCARD, recebe 20 porcento de desconto\n" +
-                      "[2] - À vista no cartão de crédito, recebe 10 porcento de desconto\n" +
-                      "[3] - Em três vezes, preço normal de etiqueta sem juros\n" +
-                      "Qual seria a forma de pagamento ?",
-                "--- --- --- --- --- --- --- --- --- ---",
-                "[1-3]");
+        choice = validaEntradaDoUsuarioSeInteiro("[1] - À vista em dinheiro/Pix ou cartão MASTERCARD, recebe 20 porcento de desconto\n" +
+                        "[2] - À vista no cartão de crédito, recebe 10 porcento de desconto\n" +
+                        "[3] - Em três vezes, preço normal de etiqueta sem juros\n" +
+                        "Qual seria a forma de pagamento ?",
+                "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---",
+                "[1-3]",
+                "Opção Invalida\nEscolha entre 1 a 4 uma das opções disponiveis para pagamento\n");
 
         return choice;
     }
